ee28a35e07eddaaff999771b60509a092544fe2f
DavLin <davlin@creatorise.com>
2022-01-14 12:07:25 +0100

    Document file management

--- Logic/File_management.cs
+++ Logic/File_management.cs
@@ -1,7 +1,15 @@
 ﻿namespace Charwriter.Logic;
 
+/// <summary>
+/// Manage read and write to files
+/// </summary>
 public static class File_management
 {
+    /// <summary>
+    /// Read all content from file
+    /// </summary>
+    /// <param name="file_name">File name</param>
+    /// <returns>Content</returns>
     public static string read(string file_name)
     {
         FileStream file_stream_in = new FileStream(file_name, FileMode.Open, FileAccess.Read);
@@ -13,6 +21,11 @@ public static class File_management
         return content;
     }
 
+    /// <summary>
+    /// Write content to file
+    /// </summary>
+    /// <param name="file_name">File name</param>
+    /// <param name="content">Content</param>
     public static void write(string file_name, string content)
     {
         FileStream file_stream_out = new FileStream(file_name, FileMode.OpenOrCreate, FileAccess.Write);
